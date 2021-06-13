7714e2cf7ced696feaec9ae235d761b151d74914
StefanyTodoroff <72222524+StefanyTodoroff@users.noreply.github.com>
2021-06-13 11:13:39 +0300

    Update MazeSolver.java

--- MazeSolver.java
+++ MazeSolver.java
@@ -1,13 +1,13 @@
-package edu.smg;
-
+package maze;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Scanner;
 
 public class MazeSolver {
-
-	// 0 = wall
-	// 1 = path
-	// 2 = destination
-
-
+	
 	public static boolean solveMaze(Maze m) {
 		Position p = m.start;
 		m.path.push(p);
@@ -15,16 +15,19 @@ public class MazeSolver {
 		while (true) {
 			int y = m.path.peek().y;
 			int x = m.path.peek().x;
-			
-			if(m.maze[y][x] != m.maze[p.y][p.x]) {
-				m.maze[y][x] = 3; 
+
+			if(m.maze[y][x]!=0) {
+			   m.maze[y][x] = 7; // marking the positions I have already been on with zero, so we avoid going in
+				// circles
 			}
 
 			// down
 			if (isValid(y + 1, x, m)) {
 				if (m.maze[y + 1][x] == 2) {
+					System.out.println("Moved down");
 					return true;
 				} else if (m.maze[y + 1][x] == 1) {
+					System.out.println("Moved down");
 					m.path.push(new Position(y + 1, x));
 					continue;
 				}
@@ -33,8 +36,10 @@ public class MazeSolver {
 			// left
 			if (isValid(y, x - 1, m)) {
 				if (m.maze[y][x - 1] == 2) {
+					System.out.println("Moved left");
 					return true;
 				} else if (m.maze[y][x - 1] == 1) {
+					System.out.println("Moved left");
 					m.path.push(new Position(y, x - 1));
 					continue;
 				}
@@ -43,8 +48,10 @@ public class MazeSolver {
 			// up
 			if (isValid(y - 1, x, m)) {
 				if (m.maze[y - 1][x] == 2) {
+					System.out.println("Moved up");
 					return true;
 				} else if (m.maze[y - 1][x] == 1) {
+					System.out.println("Moved up");
 					m.path.push(new Position(y - 1, x));
 					continue;
 				}
@@ -53,14 +60,17 @@ public class MazeSolver {
 			// right
 			if (isValid(y, x + 1, m)) {
 				if (m.maze[y][x + 1] == 2) {
+					System.out.println("Moved right");
 					return true;
 				} else if (m.maze[y][x + 1] == 1) {
+					System.out.println("Moved right");
 					m.path.push(new Position(y, x + 1));
 					continue;
 				}
 			}
 
 			m.path.pop(); // going back in case we have reached a dead end
+			System.out.println("Moved back");
 			if (m.path.size() <= 0) { // nowhere new to go
 				return false;
 			}
@@ -75,3 +85,4 @@ public class MazeSolver {
 	}
 
 }
+
