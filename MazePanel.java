98b78b45654b8cf73dbf134ec6a6c72e70e8721d
StefanyTodoroff <72222524+StefanyTodoroff@users.noreply.github.com>
2021-06-13 11:15:12 +0300

    Update MazePanel.java

--- MazePanel.java
+++ MazePanel.java
@@ -1,80 +1,77 @@
-package edu.smg;
-
-import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Graphics;
-import java.awt.Graphics2D;
 import java.io.FileNotFoundException;
-
-import javax.swing.JOptionPane;
 import javax.swing.JPanel;
-
-public class MazePanel extends JPanel{
-	boolean displayPath = false;
-	int margin = 3;
-	boolean pathFound = false;
-	
-	Maze m;
-	public MazePanel() {
-		try {
-			m = new Maze("C:\\Users\\Yaniiiiiii\\11e\\PracticeTasks_15and16\\src\\edu\\smg\\mazes.txt");
-			if (MazeSolver.solveMaze(m)) {
-				pathFound = true;
-			} 
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		}
-	}
-	
-	public void findPath() {
-		displayPath = true;
-		if(!pathFound) {
-			JOptionPane.showMessageDialog(null, "java.io.PathNotFoundException", "Error", 0);
-		}
-		repaint();
-	}
-	
-	public void clearPath() {
-		displayPath = false;
-		repaint();
-	}
-	
-	protected void paintComponent(Graphics g) {
-		super.paintComponent(g);
-		int width = getWidth() / m.maze[0].length - margin;
-		int height = getHeight() / m.maze.length - margin;
-		Graphics2D g2d = (Graphics2D)g;
-		g2d.setStroke(new BasicStroke(2));
-		for(int i = 0; i < m.maze.length; i++) {
-			for(int j = 0; j < m.maze[i].length; j++) {
-				int x = margin + j * (width + margin);
-				int y = margin + i * (height + margin);
-				if(i == m.start.y && j == m.start.x) {
-					g.setColor(Color.GREEN);
-					g.fillRect(x, y, width, height);
-				} else if(m.maze[i][j] == 1 || m.maze[i][j] == 3) {
-					g.setColor(Color.WHITE);
-					g.fillRect(x, y, width, height);
-				} else if(m.maze[i][j] == 0) {
-					g.setColor(Color.LIGHT_GRAY);
-					g.fillRect(x, y, width, height);
-					g2d.setColor(Color.BLACK);
-					g2d.drawLine(x, y, x + width, y + height);
-					g2d.drawLine(x + width, y, x, y + height);
-				} else if(m.maze[i][j] == 2) {
-					g.setColor(Color.RED);
-					g.fillRect(x, y, width, height);
-				}
-			}
-		}
-		if(displayPath) {
-			for(Position p: m.path) {
-				g.setColor(Color.GREEN);
-				g.fillRect(margin + p.x * (width + margin), margin + p.y * (height + margin), width, height);
-			}
-			
-				
-		}
-		
-	}
+public class MazePanel extends JPanel {
+ boolean displayPath = false;
+ Maze m;
+ int margin = 3;
+ public MazePanel() {
+ try {
+ m = new Maze("src/mazes.txt");
+ for (int i = 0; i < m.maze.length; i++) {
+ for (int j = 0; j < m.maze[i].length; j++) {
+ System.out.print(m.maze[i][j]);
+ }
+ System.out.println();
+ }
+ if (MazeSolver.solveMaze(m)) {
+ System.out.println("You won!");
+ } else {
+ System.out.println("No path");
+ }
+ for (int i = 0; i < m.maze.length; i++) {
+ for (int j = 0; j < m.maze[i].length; j++) {
+ System.out.print(m.maze[i][j]);
+ }
+ System.out.println();
+ }
+ } catch (FileNotFoundException e) {
+ // TODO Auto-generated catch block
+ e.printStackTrace();
+ }
+ }
+ public void findPath() {
+ displayPath = true;
+ repaint();
+ }
+ public void cleanPath() {
+ displayPath = false;
+ repaint();
+ }
+ protected void paintComponent(Graphics g) {
+ super.paintComponent(g);
+ g.setColor(Color.WHITE);
+ int width = getWidth() / m.maze[0].length - margin;
+ int height = getHeight() / m.maze.length - margin;
+ for (int i = 0; i < m.maze.length; i++) {
+ for (int j = 0; j < m.maze[i].length; j++) {
+ if (m.maze[i][j] == 1 || m.maze[i][j] == 7) {
+ g.fillRect(margin + j * (width + margin), margin + i * (height + margin), width, height);
+ // System.out.print(m.maze[i][j]);
+ }
+ else if (m.maze[i][j] == 2) {
+ g.setColor(Color.RED);
+ g.fillRect(margin + j * (width + margin), margin + i * (height + margin), width, height);
+ g.setColor(Color.WHITE);
+ // System.out.print(m.maze[i][j]);
+ }
+ 
+ }
+ // System.out.println();
+ }
+ if (displayPath) {
+ g.setColor(Color.BLACK);
+ for (Position p : m.path) {
+ g.fillRect(margin + p.x * (width + margin), margin + p.y * (height + margin), width, height);
+ }
+ }
+ 
+ 
+ g.setColor(Color.GREEN);
+ g.fillRect(margin + m.start.x * (width + margin), margin + m.start.y * (height + margin), width, height);
+ g.setColor(Color.WHITE);
+ // System.out.print(m.maze[i][j]);
+ 
+ }
 }
